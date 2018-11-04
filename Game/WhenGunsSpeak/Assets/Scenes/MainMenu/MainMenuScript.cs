3d72ff4ab94f7cf4b74328df9acb75b93b4f449e
Roman Boykov <gendalf90@outlook.com>
2018-11-04 20:30:56 +0300

    game

---
+++ Game/WhenGunsSpeak/Assets/Scenes/MainMenu/MainMenuScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu.Main
+{
+    public class MainMenuScript : MonoBehaviour
+    {
+        public void MultiplayerMenu()
+        {
+            SceneManager.LoadScene("MultiplayerMenu", LoadSceneMode.Single);
+        }
+
+        public void Quit()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Game/WhenGunsSpeak/Assets/Scenes/MultiplayerMenu/MultiplayerMenuScript.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu.Multiplayer
+{
+    public class MultiplayerMenuScript : MonoBehaviour
+    {
+        public void MainMenu()
+        {
+            SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+        }
+    }
+}
